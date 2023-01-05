e130b7a775e3ff27621cba6b37392a70e66ab06f
NikolayAngelov <103751145+NikolayAngelov96@users.noreply.github.com>
2023-01-05 16:47:43 +0200

    update assets loading

--- src/utils/loadAssets.ts
+++ src/utils/loadAssets.ts
@@ -1,5 +1,7 @@
-import { Assets } from "pixi.js";
+import { Assets, BaseTexture } from "pixi.js";
 import type { ResolverManifest } from "pixi.js";
+
+type TilesType = 'bevel' | 'hover' | 'inset';
 
 export async function loadAssets() {
     const manifest: ResolverManifest = {
@@ -19,5 +21,16 @@ export async function loadAssets() {
 
     const bundleIds = manifest.bundles.map(bundle => bundle.name);
 
-    return await Assets.loadBundle(bundleIds);
+    const assets = await Assets.loadBundle(bundleIds);
+    return mapAssets(assets);
+};
+
+export function mapAssets(assets): Map<TilesType, BaseTexture> {
+    const map = new Map<TilesType, BaseTexture>();
+
+    map.set('bevel', assets.tiles.bevel);
+    map.set('hover', assets.tiles.hover);
+    map.set('inset', assets.tiles.inset);
+
+    return map;
 }
